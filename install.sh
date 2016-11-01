5d63e8e29c981306ca4469a87638b42137097d64
Timvde <tim_vdeynde@hotmail.com>
2016-11-01 22:28:22 +0100

    Added install script. Did not actually test this.

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+PWD=$(pwd)
+
+echo "Linking files"
+
+ln -s "${PWD}/.bash_aliases" ~/.bash_aliases
+ln -s "${PWD}/.tmux.conf" ~/.tmux.conf
+ln -s "${PWD}/.vim" ~/.vim
+ln -s "${PWD}/.vimrc" ~/.vimrc
+ln -s "${PWD}/.zshrc" ~/.zshrc
+ln -s "${PWD}/.gitignore_global" ~/.gitignore_global
+ln -s "${PWD}/.gitconfig" ~/.gitconfig
+
+echo "Linking done"
+
+echo "Compiling cpsm"
+cd .vim/bundle/cpsm
+./install.sh
+cd -
+echo "Compiling cpsm done"
