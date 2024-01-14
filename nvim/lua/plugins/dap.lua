54a8def2e4f82382c0e0404b206ed072901b7ddc
Tim Van den Eynde <tim.vandeneynde@mobilevikings.be>
2024-01-14 10:11:29 +0100

    Always open dap-ui in a new tab

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -13,7 +13,49 @@ return {
 			{ "<leader>dc", function() require("dap").continue() end },
 		},
 		config = function()
-			require("dapui").setup()
+			local dapui = require("dapui")
+			dapui.setup()
+			local dap = require("dap")
+			local debug_win = nil
+			local debug_tab = nil
+			local debug_tabnr = nil
+
+			local function open_in_tab()
+				if debug_win and vim.api.nvim_win_is_valid(debug_win) then
+					vim.api.nvim_set_current_win(debug_win)
+					return
+				end
+
+				vim.cmd('tabedit %')
+				debug_win = vim.fn.win_getid()
+				debug_tab = vim.api.nvim_win_get_tabpage(debug_win)
+				debug_tabnr = vim.api.nvim_tabpage_get_number(debug_tab)
+
+				dapui.open()
+			end
+
+			local function close_tab()
+				dapui.close()
+
+				if debug_tab and vim.api.nvim_tabpage_is_valid(debug_tab) then
+					vim.api.nvim_exec('tabclose ' .. debug_tabnr, false)
+				end
+
+				debug_win = nil
+				debug_tab = nil
+				debug_tabnr = nil
+			end
+
+			-- Attach DAP UI to DAP events
+			dap.listeners.after.event_initialized['dapui_config'] = function()
+				open_in_tab()
+			end
+			dap.listeners.before.event_terminated['dapui_config'] = function()
+				close_tab()
+			end
+			dap.listeners.before.event_exited['dapui_config'] = function()
+				close_tab()
+			end
 		end
 	},
 	{
