11e31ac6a836187fb8174260bbab4e55af03ac57
Tim Van den Eynde <tim.vandeneynde@mobilevikings.be>
2024-12-01 18:41:59 +0100

    Use zf-native for telescope
    This is faster and has a better file matcher.

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -4,6 +4,7 @@ return {
 		"nvim-lua/plenary.nvim",
 		"debugloop/telescope-undo.nvim",
 		"AckslD/nvim-neoclip.lua",
+		"natecraddock/telescope-zf-native.nvim",
 	},
 	cmd = { "Telescope" },
 	keys = {
@@ -37,10 +38,48 @@ return {
 					},
 				},
 				neoclip = {},
+				["zf-native"] = {
+					-- options for sorting file-like items
+					file = {
+						-- override default telescope file sorter
+						enable = true,
+
+						-- highlight matching text in results
+						highlight_results = true,
+
+						-- enable zf filename match priority
+						match_filename = true,
+
+						-- optional function to define a sort order when the query is empty
+						initial_sort = nil,
+
+						-- set to false to enable case sensitive matching
+						smart_case = true,
+					},
+
+					-- options for sorting all other items
+					generic = {
+						-- override default telescope generic item sorter
+						enable = true,
+
+						-- highlight matching text in results
+						highlight_results = true,
+
+						-- disable zf filename match priority
+						match_filename = false,
+
+						-- optional function to define a sort order when the query is empty
+						initial_sort = nil,
+
+						-- set to false to enable case sensitive matching
+						smart_case = true,
+					},
+				},
 			},
 		})
 		require("telescope").load_extension("undo")
 		require("telescope").load_extension("neoclip")
+		require("telescope").load_extension("zf-native")
 		require("neoclip").setup()
 	end,
 }
