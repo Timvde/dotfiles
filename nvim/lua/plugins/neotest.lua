576c54efd35bf1245ab05914ddf8fb2a5e41ab0d
Tim Van den Eynde <tim.vandeneynde@mobilevikings.be>
2024-12-01 18:39:09 +0100

    Update config for latest neotest
    nvim-nio is a new dependency. The other change is just autoformat.

--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -2,15 +2,16 @@ return {
 	{
 		"nvim-neotest/neotest",
 		dependencies = {
+			"nvim-neotest/nvim-nio",
 			"nvim-lua/plenary.nvim",
 			"nvim-treesitter/nvim-treesitter",
 			"antoinemadec/FixCursorHold.nvim"
 		},
 		keys = {
-			{"<leader>ts", function() require("neotest").summary.toggle({ enter = true }) end, desc="Toggle neotest sidebar" },
-			{ "<leader>fi", function() require("neotest").run.run(vim.fn.expand("%"))  end, desc="Run all tests in this file" },
-			{ "<leader>fu", function() require("neotest").run.run() end, desc="Run the current function" },
-			{ "<leader>fd", function() require("neotest").run.run({strategy = "dap"})  end, desc="Debug the current function" },
+			{ "<leader>ts", function() require("neotest").summary.toggle({ enter = true }) end, desc = "Toggle neotest sidebar" },
+			{ "<leader>fi", function() require("neotest").run.run(vim.fn.expand("%")) end,      desc = "Run all tests in this file" },
+			{ "<leader>fu", function() require("neotest").run.run() end,                        desc = "Run the current function" },
+			{ "<leader>fd", function() require("neotest").run.run({ strategy = "dap" }) end,    desc = "Debug the current function" },
 		},
 		config = function()
 			require("neotest").setup({
